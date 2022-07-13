274a43a55905fd6fa53ce270dd016898297db1dc
Sreynaj Keb <kebsreynaj2@gmail.com>
2022-07-13 17:27:41 +0700

    sourcecode

--- no_sort_list.py
+++ no_sort_list.py
@@ -1,12 +1,19 @@
 
 L = [1,4,7,5,9,3,0,3,2]
 for i in range(len(L)):
+    #accending
     for n in range(i + 1, len(L)):
 
         if L[i] > L[n]:
             L[i], L[n] = L[n], L[i]
-
-print(L)
+print("Accending: ", L)
+#decending
+for i in range(len(L)):
+    for n in range(i + 1, len(L)):
+    
+        if L[i] < L[n]:
+            L[i], L[n] = L[n], L[i]
+print("Decending: ", L)
 
 #another way 
 data_list = [-5, -23, 5, 0, 23, -6, 23, 67]
@@ -15,8 +22,8 @@ new_list = []
 while data_list:
     minimum = data_list[0]
     for i in data_list:
-        if minimum > i:
+        if minimum > i: #min < i: accending, min > i :decending
             minimum = i
     new_list.append(minimum)
     data_list.remove(minimum)
-print(new_list)
+print("Accending" , new_list)
